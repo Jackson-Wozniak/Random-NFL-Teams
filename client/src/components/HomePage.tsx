00285423ed490dd6cceb9c897e685c6ebb4284e5
Jackson Wozniak <jjw30@comcast.net>
2025-10-10 12:00:18 -0400

    initial client files

---
+++ client/src/components/HomePage.tsx
@@ -0,0 +1,8 @@
+
+export const HomePage: React.FC = () => {
+    return (
+        <></>
+    )
+}
+
+export default HomePage;
---
+++ client/src/types/career.ts
@@ -0,0 +1,8 @@
+
+export interface CareerContextDto {
+
+}
+
+export interface CareerRulesDto {
+    
+}
